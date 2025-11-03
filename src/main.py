752522d7d682d65b237739f6fd94a83993bf676e
hatatat-dev <hatatat@glowstonemania.com>
2025-11-02 18:51:21 -0800

    [autonomous] drive forward 240 mm

--- src/main.py
+++ src/main.py
@@ -24,36 +24,36 @@ def autonomous_function():
     log(("Competition", "competition"), "autonomous_begin")
 
     robot_position.reset(Position(1200, 1200))
-    reset_heading_to_aim(Position(1250, 1200), FORWARD)
+    reset_heading_to_aim(Position(1440, 1200), FORWARD)
     # reset_robot_position_and_heading_to_gps()
 
     # robot should be 77.5 cm from both sides
 
     matchload.set(True)
-    trigger_mover.move(Position(1250, 1200), FORWARD)
+    trigger_mover.move(Position(1440, 1200), FORWARD)
 
     conveyor.spin(FORWARD, FORWARD, FORWARD)
     wait(6000, MSEC)
     conveyor.spin(STOP, STOP, STOP)
 
-    # Steps below are just for testing out the SVG generation
-    
-    matchload.set(False)
+    # # Steps below are just for testing out the SVG generation
 
-    trigger_mover.move(Position(1200, 1200), REVERSE)
-    trigger_turner.turn(180, FRAME_HEADING_RELATIVE)
-    slow_trigger_mover.move(Position(800, 1200), FORWARD)
+    # matchload.set(False)
 
-    conveyor.spin(REVERSE, REVERSE, FORWARD)
-    wait(6000, MSEC)
-    conveyor.spin(STOP, STOP, STOP)
+    # trigger_mover.move(Position(1200, 1200), REVERSE)
+    # trigger_turner.turn(180, FRAME_HEADING_RELATIVE)
+    # slow_trigger_mover.move(Position(800, 1200), FORWARD)
 
-    conveyor.spin(FORWARD, REVERSE, FORWARD)
-    trigger_mover.move(Position(1200, -600), FORWARD)
+    # conveyor.spin(REVERSE, REVERSE, FORWARD)
+    # wait(6000, MSEC)
+    # conveyor.spin(STOP, STOP, STOP)
 
-    conveyor.spin(FORWARD, REVERSE, REVERSE)
+    # conveyor.spin(FORWARD, REVERSE, FORWARD)
+    # trigger_mover.move(Position(1200, -600), FORWARD)
 
-    trigger_mover.move(Position(-1200, -600), FORWARD)
+    # conveyor.spin(FORWARD, REVERSE, REVERSE)
+
+    # trigger_mover.move(Position(-1200, -600), FORWARD)
 
     log(("Competition", "competition"), "autonomous_end")
 
